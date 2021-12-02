ddbe563dd910a31ef6a4407660d08ab4b85ff72d
Alexis Le Provost <alexis.leprovost@outlook.com>
2021-12-02 20:16:56 +0100

    Use core library within day1 puzzle

--- advent-of-code-day1/src/main.rs
+++ advent-of-code-day1/src/main.rs
@@ -1,18 +1,8 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
-use std::str::FromStr;
 
-use eyre::Result;
 use structopt::StructOpt;
-use strum::EnumString;
 
-#[derive(EnumString)]
-#[strum(serialize_all = "lowercase")]
-enum Puzzle {
-  Part1,
-  Part2,
-}
+use aoc_core::{Puzzle, Result};
 
 #[derive(StructOpt)]
 struct Opt {
@@ -24,26 +14,11 @@ struct Opt {
   debug: bool,
 }
 
-fn read_lines_as<T>(filename: PathBuf) -> Result<Vec<T>>
-where
-  T: FromStr,
-  T::Err: Into<eyre::Error>,
-{
-  let file = File::open(filename)?;
-  let reader = BufReader::new(file);
-
-  reader
-    .lines()
-    .map(|line| line.map(|x| x.parse::<T>().map_err(Into::into)))
-    .flatten()
-    .collect::<Result<Vec<_>>>()
-}
-
 fn main() -> Result<()> {
   color_eyre::install()?;
 
   let opt = Opt::from_args();
-  let depths = read_lines_as::<usize>(opt.filename)?;
+  let depths = aoc_core::read_lines_as::<usize>(opt.filename)?;
 
   let depths = match opt.puzzle {
     Puzzle::Part1 => depths,
